f2d89d083d70b52d520615530f540320eb456a71
adesinaisaiah100 <166018187+adesinaisaiah100@users.noreply.github.com>
2026-01-22 19:35:28 +0100

    feat: implement hero section with call to action

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,41 @@
 import Image from "next/image";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <main className="flex bg-[#FDFBF7] min-h-screen flex-col items-center font-nunito">
+      <div className="w-full p-2 h-screen flex flex-col relative">
+      <Navbar />
+      <div className="absolute top-0 left-0 w-full h-full flex justify-between px-10 items-center">
+        <div className="w-full flex justify-between items-center">
+        <div className="max-w-4xl flex flex-col gap-3 ">
+          <h1 className="text-2xl md:text-7xl font-nunito font-bold leading-tight text-[#2E1A47#2E1A47]">
+            You Deserve <span className="text-[#feb10a] font-lobster">Empowerment.</span> You Need Recognition.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          <p className="text-lg font-nunito text-[#2E1A47]">When you need us, we are here with hearts and hands you can trust</p>
+          <div>
+             <div className=" text-sm italic text-gray-600 mb-10 mt-1">
+            Respect • Support • Encouragement • Dignity • Protection
+          </div>
+            <button className="w-45 font-nunito  h-12 mt-2 bg-[#FFB81C] rounded-2xl hover:bg-[#fbb00e] transition-colors mr-4">
+              Help
+            </button>
+            
+          </div>
+         
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+               <div className="relative w-full max-w-md h-[380px] md:h-[550px] rounded-[3rem] overflow-hidden shadow-2xl border-4 border-white/80 ring-1 ring-gray-100/50 transform rotate-2 hover:rotate-0 transition-transform duration-500">
+                <Image
+                  src="/woman3.png"
+                  alt="Empowered Woman"
+                  fill
+                  className="object-cover"
+                  priority
+                />
+             </div>
+      </div>
+      </div>
+      </div>
+    </main>
   );
 }
